27b2cd44787b60943f8cae7a94c7f4524e679ffc
abstxn <108669111+abstxn@users.noreply.github.com>
2023-05-12 19:17:26 +0800

    Build entry lookup functionality

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # toggl-polybar
 
 A simple [Polybar](https://github.com/polybar/polybar) widget to display [Toggl Track](https://toggl.com/track/) status.
+
+# How It Works
+
+Using your Toggl Track account's API token, perform authentication with Toggl Track.
+
+A successful authentication results in a *session*, represented by a *cookie*.
+
--- toggl.py
+++ toggl.py
@@ -1,7 +1,27 @@
+import requests
+import json
+import time
 from dotenv import dotenv_values
+from base64 import b64encode
 
-API_TOKEN = dotenv_values('api_token.env')['API_TOKEN']
-if API_TOKEN == None:
-    print('No API token loaded. Exiting...')
-    exit(1)
+def prettify_duration(seconds) -> str:
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    return '{:02.0f}h {:02.0f}m {:02.0f}s'.format(hours, minutes, seconds)
 
+def get_api_token(env_path) -> str:
+    token_str = dotenv_values(env_path)['API_TOKEN']
+    return b64encode((token_str + ':api_token').encode('utf-8')).decode('ascii')
+
+API_TOKEN = get_api_token('api_token.env')
+current_entry_url = 'https://api.track.toggl.com/api/v9/me/time_entries/current'
+headers = {'content-type': 'application/json', 'Authorization' : 'Basic %s' %  API_TOKEN}
+
+data = requests.get(current_entry_url, headers=headers).json()
+
+if data == None:
+    print("No running project.")
+else:
+    entry_description = data['description']
+    entry_duration = prettify_duration(time.time() + data['duration'])
+    print(f'{entry_description} {entry_duration}')
