27258c4b5b540d9f3e3e9b4734eb154af14e8603
abstxn <108669111+abstxn@users.noreply.github.com>
2023-05-12 19:36:48 +0800

    Fix improper path of .env file

--- toggl.py
+++ toggl.py
@@ -3,17 +3,21 @@ import json
 import time
 from dotenv import dotenv_values
 from base64 import b64encode
+from os import path
 
 def prettify_duration(seconds) -> str:
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
-    return '{:02.0f}h {:02.0f}m {:02.0f}s'.format(hours, minutes, seconds)
+    return '{:02.0f}h {:02.0f}m'.format(hours, minutes)
+    # return '{:02.0f}h {:02.0f}m {:02.0f}s'.format(hours, minutes, seconds)
 
 def get_api_token(env_path) -> str:
     token_str = dotenv_values(env_path)['API_TOKEN']
     return b64encode((token_str + ':api_token').encode('utf-8')).decode('ascii')
 
-API_TOKEN = get_api_token('api_token.env')
+script_dir = path.dirname(path.abspath(__file__))
+
+API_TOKEN = get_api_token(script_dir + '/api_token.env')
 current_entry_url = 'https://api.track.toggl.com/api/v9/me/time_entries/current'
 headers = {'content-type': 'application/json', 'Authorization' : 'Basic %s' %  API_TOKEN}
 
@@ -24,4 +28,4 @@ if data == None:
 else:
     entry_description = data['description']
     entry_duration = prettify_duration(time.time() + data['duration'])
-    print(f'{entry_description} {entry_duration}')
+    print(f'{entry_description}: {entry_duration}')
